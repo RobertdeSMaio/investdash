6ccd01f3483060f459da2bf792ffd5ee9aa3dea0
Robert de S. Maio <robertmaio@outlook.com>
2026-01-27 22:11:30 -0300

    add home

--- src/componentes/Investiments.tsx
+++
@@ -1,3 +0,0 @@
-export default function Invest() {
-  return <div className="p-6 bg-white m-10 rounded-lg shadow-md"></div>;
-}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import Dash from "../componentes/dash";
-import Invest from "../componentes/Investiments";
 import NavBar from "../componentes/NavBar";
 
 export default function Home() {
@@ -10,16 +9,7 @@ export default function Home() {
 
       <main className="min-h-screen bg-gray-100 p-8 flex flex-col gap-4">
         <div className="bg-sky-300 p-6 rounded-lg flex-0">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <div className="bg-sky-200 aspect-square rounded-lg shadow-sm">
-              <div>
-                <h1 className="flex-1 p-6">Investiments</h1>
-                <div>
-                  <Invest />
-                </div>
-              </div>
-            </div>
-
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="bg-sky-200 aspect-square rounded-lg shadow-sm">
               <h1 className="p-6">Dashboard - investiments</h1>
               <Dash />
@@ -29,13 +19,18 @@ export default function Home() {
               <h1 className="p-6">Calculators</h1>
               <h3></h3>
               <Link to="/ComposeJ">
-                <div className="bg-sky-100 p-20 flex-1 m-8 rounded-lg shadow-sm hover:bg-sky-300 transition-colors">
+                <div className="bg-sky-100 p-15 flex-1 m-8 rounded-lg shadow-sm hover:bg-sky-300 transition-colors">
                   Juros Compostos
                 </div>
               </Link>
               <Link to="/SimpleJ">
-                <div className="bg-sky-100 p-20 flex-1 m-8 rounded-lg shadow-sm hover:bg-sky-300 transition-colors">
+                <div className="bg-sky-100 p-15 flex-1 m-8 rounded-lg shadow-sm hover:bg-sky-300 transition-colors">
                   Juros Simples
+                </div>
+              </Link>
+              <Link to="/Invest">
+                <div className="bg-sky-100 p-15 flex-1 m-8 rounded-lg shadow-sm hover:bg-sky-300 transition-colors">
+                  Investimentos
                 </div>
               </Link>
             </div>
