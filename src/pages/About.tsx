1789452c07949ec537b76978da2b709392fe6ef5
Robert de S. Maio <robertmaio@outlook.com>
2026-03-22 09:01:29 -0300

    edit about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,13 @@
 export default function About() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 bg-sky-300 gap-6 p-6 rounded-lg">
-      Criei o site para treinar meu desenvolvimento e salvar meus investimentos
-      e parar de usar planilha
+    <div className="grid grid-cols-1 bg-sky-300 gap-6 p-6 rounded-lg">
+      <div className="bg-sky-200 p-6 rounded-lg">
+        <p>
+          Criei o site para treinar meu desenvolvimento e salvar meus
+          investimentos e parar de usar planilha
+        </p>
+        <p>Facilitando e retirando o uso da planilha, para tirar as formulas</p>
+      </div>
     </div>
   );
 }
