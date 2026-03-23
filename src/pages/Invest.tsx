054f62da6a31c909efbe462edc7b5c6f9dd3a363
Robert de S. Maio <robertmaio@outlook.com>
2026-03-23 11:55:48 -0300

    start to implement inv page

--- src/pages/Invest.tsx
+++ src/pages/Invest.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
 export default function Invest() {
+  const [addInv, setAddInv] = useState(null);
+
   return (
     <>
-      <div></div>
+      <div className="shadow-md rounded-lg p-1 bg-sky-50">
+        <label htmlFor="inv"></label>
+        <input
+          className="p-2 border rounded-lg"
+          type="inv"
+          placeholder="Seu investimento"
+        />
+        <button
+          className="m-2 rounded-lg p-1 border hover:bg-sky-200"
+          onClick={() => setAddInv}
+        >
+          Insira
+        </button>
+      </div>
+      <div className="shadow-md rounded-lg p-1 mt-3 bg-sky-50">
+        /*TODO lista de investimentos */
+      </div>
     </>
   );
 }
