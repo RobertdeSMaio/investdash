e7a9c17ccfacc6893ab4da1b475fea394686d1fa
Robert de S. Maio <robertmaio@outlook.com>
2026-04-20 19:51:37 -0300

    api.ts pulling the wrong var

--- src/services/api.ts
+++ src/services/api.ts
@@ -19,7 +19,7 @@ api.interceptors.response.use(
     if (
       error.response?.status === 401 &&
       !original._retry &&
-      !original.url?.includes("/api/auth/refresh")
+      !original.url?.includes("/auth/refresh")
     ) {
       original._retry = true;
       const refreshToken = localStorage.getItem("refreshToken");
@@ -32,7 +32,7 @@ api.interceptors.response.use(
 
       try {
         const { data } = await axios.post(
-          `${import.meta.env.VITE_API_URL}/api/auth/refresh`,
+          `${import.meta.env.VITE_API_URL}/auth/refresh`,
           { refreshToken },
         );
 
