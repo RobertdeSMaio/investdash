3ee56f53f8fdc6d6a74ae9db4737c306049f9df2
Robert de S. Maio <robertmaio@outlook.com>
2026-04-20 02:12:00 -0300

    Name changed

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { AppRoutes } from "./routes/Routes";
+import { AppRoutes } from "./routes/Route";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
